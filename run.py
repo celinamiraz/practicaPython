7ba0b510ebd9da309257e11ee6b5e903e4c80b37
Celina <celinamiraz@gmail.com>
2021-03-17 23:04:34 -0300

    Creacion del archivo inicial: Responde si es mayuscula o minuscula

---
+++ run.py
@@ -0,0 +1,10 @@
+#Dado una letra ingresada por el teclado, queremos saber si es mayúscula o minúscula.
+letra = input("Por favor ingresar una letra")
+while len(letra) != 1:
+    letra =input("Ingresar UNA sola letra")
+if letra > "a" and letra < "z":
+    print(f"La letra ingresada {letra} es minuscula")
+elif letra > "A" and letra < "Z":
+    print(f"La letra ingresada {letra} es mayuscula")
+else:
+    print(f"El caracter ingresado {letra} no es una letra")
